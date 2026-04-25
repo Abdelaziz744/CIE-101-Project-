48af5075bdae7d7e4a78969431df0236d2444190
Megz <s-mohamed.abdeltawab@zewailcity.edu.eg>
2026-04-25 23:34:45 +0300

    Refactor Game class and add food area management

--- Core/Game.cpp
+++ Core/Game.cpp
@@ -266,3 +266,168 @@ void Game::go() const
 
 	} while (!isExit);
 }
+//magdy
+#include "Game.h"
+#include "../Config/GameConfig.h"
+#include "../UI/Warehouse.h"
+#include "../Entities/Animal.h"
+#include "../UI/BudgetBar.h"
+#include <ctime>
+#include <iostream>
+using namespace std;
+void Game::createFoodAreas()
+{
+	int startY = config.toolBarHeight * 2 + 40;
+
+	int positions[3][3] = {
+		{ 100, startY,       15 },
+		{ 350, startY + 140, 20 },
+		{ 600, startY + 40,  10 },
+	};
+
+	for (int i = 0; i < 3; i++)
+	{
+		point p;
+		p.x = positions[i][0];
+		p.y = positions[i][1];
+		foodAreas[numFoodAreas] = new FoodArea(this, p, 100, 60, positions[i][2]);
+		foodAreas[numFoodAreas]->draw();
+		numFoodAreas++;
+	}
+}
+
+void Game::gameTick()
+{
+	// Decrease food areas once per tick
+	for (int i = 0; i < numFoodAreas; i++)
+		if (foodAreas[i] && foodAreas[i]->isActive)
+			foodAreas[i]->decreaseFood();
+
+	// Tick chick production
+	if (gameBudgetbar)
+		gameBudgetbar->tickChicks(warehouse);
+
+	tickCount++;
+}
+
+void Game::go()
+{
+	pWind->ChangeTitle("- - - Farm Frenzy (CIE101-project) - - -");
+
+	bool isExit = false;
+	time_t lastTick = time(nullptr);
+
+	while (!isExit)
+	{
+		string budget_string = "BUDGET = $" + to_string(budget);
+		printBudget(budget_string);
+		printMessage("Click an animal icon to buy it!");
+
+		int x, y;
+		getMouseClick(x, y);
+
+		// Handle click
+		if (y >= 0 && y < config.toolBarHeight)
+		{
+			isExit = gameToolbar->handleClick(x, y);
+		}
+		else if (y >= config.toolBarHeight && y < 2 * config.toolBarHeight)
+		{
+			gameBudgetbar->handleClick(x, y);
+		}
+
+		// Tick once per second based on real time
+		time_t now = time(nullptr);
+		if (now - lastTick >= 1)
+		{
+			gameTick();
+			lastTick = now;
+		}
+	}
+}
+
+clicktype Game::getMouseClick(int& x, int& y) const
+{
+	return pWind->WaitMouseClick(x, y);
+}
+
+string Game::getSrting() const
+{
+	string Label;
+	char Key;
+	keytype ktype;
+	pWind->FlushKeyQueue();
+	while (1)
+	{
+		ktype = pWind->WaitKeyPress(Key);
+		if (ktype == ESCAPE) return "";
+		if (Key == 13) return Label;
+		if (Key == 8)
+		{
+			if (Label.size() > 0) Label.resize(Label.size() - 1);
+			else Key = '\0';
+		}
+		else Label += Key;
+		printMessage(Label);
+	}
+}
+
+window* Game::CreateWind(int w, int h, int x, int y) const
+{
+	window* pW = new window(w, h, x, y);
+	pW->SetBrush(config.bkGrndColor);
+	pW->SetPen(config.bkGrndColor, 1);
+	pW->DrawRectangle(0, 0, w, h);
+	return pW;
+}
+
+void Game::createToolbar()
+{
+	point p; p.x = 0; p.y = 0;
+	gameToolbar = new Toolbar(this, p, 0, config.toolBarHeight);
+	gameToolbar->draw();
+}
+
+void Game::createBudgetbar()
+{
+	point p; p.x = 0; p.y = config.toolBarHeight;
+	gameBudgetbar = new Budgetbar(this, p, 0, config.toolBarHeight);
+	gameBudgetbar->draw();
+}
+
+void Game::clearBudget() const
+{
+	pWind->SetPen(config.bkGrndColor, 1);
+	pWind->SetBrush(config.bkGrndColor);
+	pWind->DrawRectangle(config.windWidth - 500, config.toolBarHeight,
+		config.windWidth, 2 * config.toolBarHeight);
+}
+
+void Game::printBudget(string msg) const
+{
+	clearBudget();
+	pWind->SetPen(config.penColor, 50);
+	pWind->SetFont(24, BOLD, BY_NAME, "Arial");
+	pWind->DrawString(config.windWidth - 200, config.toolBarHeight + 10, msg);
+}
+
+void Game::clearStatusBar() const
+{
+	pWind->SetPen(config.statusBarColor, 1);
+	pWind->SetBrush(config.statusBarColor);
+	pWind->DrawRectangle(0, config.windHeight - config.statusBarHeight,
+		config.windWidth, config.windHeight);
+}
+
+void Game::printMessage(string msg) const
+{
+	clearStatusBar();
+	pWind->SetPen(config.penColor, 50);
+	pWind->SetFont(24, BOLD, BY_NAME, "Arial");
+	pWind->DrawString(10, config.windHeight - (int)(0.85 * config.statusBarHeight), msg);
+}
+
+window* Game::getWind() const
+{
+	return pWind;
+}
